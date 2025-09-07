eef04af5b8f8253eb8a579d444098734cb4f0c9b
ShmuelBarazani <tropikan1@gmail.com>
2025-09-07 19:34:13 +0300

    Delete icons directory

--- icons/vite.config.js
+++
@@ -1,9 +0,0 @@
-// C:\Users\user\אפליקציות\katregel-mobile\vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: './' מאפשר לטעון את האתר ישירות מקבצים (file://) בטלפון
-export default defineConfig({
-  base: './',
-  plugins: [react()]
-})
